85776d34c219d14b56f574580234f646463a2da7
LaurentMali <l.mali@students.htl-leonding.ac.at>
2024-10-21 13:01:59 +0200

    new build.sh-file

--- minikube-pvc/build.sh
+++ minikube-pvc/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Convert repository name to lowercase
+REPO_NAME=$(echo "$GITHUB_REPOSITORY" | tr '[:upper:]' '[:lower:]')
+
 mvn -B package
 cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
-docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
-docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
+docker build --tag ghcr.io/$REPO_NAME/backend:latest ./target
+docker push ghcr.io/$REPO_NAME/backend:latest
